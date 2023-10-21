92eb4e121ffb30c277281981005e1b42c8b45b93
stilisky <setonde.hounnouvi@epitech.eu>
2023-10-21 08:31:51 +0100

    Model Layer added

--- WEBDRIVER/src/app/app.module.ts
+++ WEBDRIVER/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DriverComponent } from './driver/driver.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { DriverComponent } from './driver/driver.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- WEBDRIVER/src/app/driver/driver.component.ts
+++ WEBDRIVER/src/app/driver/driver.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-driver',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./driver.component.css']
 })
 export class DriverComponent {
+  // searchValue: string;
 
+  constructor(
+
+  ){}
+
+  onSubmit(form: NgForm){
+
+  }
 }
