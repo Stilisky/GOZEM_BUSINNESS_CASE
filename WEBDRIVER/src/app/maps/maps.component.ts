8b69efff727bb9002a8075cefbc86db8a2885a6c
stilisky <setonde.hounnouvi@epitech.eu>
2023-10-22 15:03:24 +0100

    Added Sender location in map

--- WEBDRIVER/src/app/maps/maps.component.ts
+++ WEBDRIVER/src/app/maps/maps.component.ts
@@ -1,7 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import * as L from 'leaflet';
-import { Delivery } from '../models/delivery';
-import { DeliveryService } from '../services/delivery.service';
 
 @Component({
   selector: 'app-maps',
@@ -25,6 +23,10 @@ export class MapsComponent implements OnInit {
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
+
+    L.marker([this.deliveryData.package_id.from_location.lat, this.deliveryData.package_id.from_location.lng], {}).addTo(this.map)
+      .bindPopup('A Sender address')
+      .openPopup();
 
     L.marker([this.deliveryData.package_id.to_location.lat, this.deliveryData.package_id.to_location.lng], {}).addTo(this.map)
       .bindPopup('A Recipient address')
